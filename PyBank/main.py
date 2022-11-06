c8ab737e07dc0c92f907bc506dc2f022c6e865ef
SBrindley <113051302+SBrindley@users.noreply.github.com>
2022-11-06 10:23:48 +0000

    update 1.1

---
+++ PyBank/main.py
@@ -0,0 +1,13 @@
+# Start by asking to read the file
+
+import os
+
+import csv
+
+#find the csv file to read
+
+PyBankcsv = os.path.join("Resources", "budget_data.csv")
+
+# Create the variables to hold the data needed for the report
+
+
